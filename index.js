824b4d42e37894eadfb530662187758e1b168878
Dudeicle <brian-griffiths@lambdastudents.com>
2020-08-03 16:57:06 -0400

    MVP

--- index.js
+++ index.js
@@ -32,47 +32,81 @@ let users = [
 server.get("/api/users", (req, res) => {
 	res.status(200).json(users);
 });
-
 // GET REQUEST BY ID -- WORKING
 server.get("/api/users/:id", (req, res) => {
 	const id = req.params.id;
-	let user = users.filter((a) => a.id === id);
-	res.status(200).json(user);
-});
 
+	let target = users.find((a) => a.id === id);
+
+	if (!target) {
+		res
+			.status(404)
+			.json({ message: "The user with the specified ID does not exist." });
+	} else {
+		let user = users.filter((a) => a.id === id);
+		res.status(200).json(user);
+	}
+});
 // POST REQUEST -- WORKING
 server.post("/api/users", (req, res) => {
 	const user = req.body;
 
 	user.id = shortid.generate();
 
-	users.push(user);
-
-	res.status(200).json(users);
+	if (!user.name || !user.bio) {
+		res
+			.status(400)
+			.json({ errorMessage: "Please provide name and bio for the user." });
+	} else {
+		users.push(user);
+		res.status(201).json(users);
+	}
 });
-
 // PUT REQUEST -- WORKING
 server.put("/api/users/:id", (req, res) => {
 	const id = req.params.id;
 	const changes = req.body;
 
+	let target = users.find((a) => a.id === id);
 	let found = users.find((a) => a.id === id);
 
-	if (found) {
+	if (!target) {
+		res
+			.status(404)
+			.json({ message: "The user with the specified ID does not exist." });
+	} else if (!changes.name || !changes.bio) {
+		res
+			.status(400)
+			.json({ errorMessage: "Please provide name and bio for the user." });
+	} else if (found) {
 		Object.assign(found, changes);
 
 		res.status(200).json(users);
-	} else {
-		res.status(404).json({ message: `There is no account with id ${id}` });
 	}
-});
 
+	// if (found) {
+	// 	Object.assign(found, changes);
+
+	// 	res.status(200).json(users);
+	// } else {
+	// 	res
+	// 		.status(404)
+	// 		.json({ message: "The user with the specified ID does not exist." });
+	// }
+});
 // DELETE REQUEST -- WORKING
 server.delete("/api/users/:id", (req, res) => {
 	const id = req.params.id;
+	let deleteTarget = users.find((a) => a.id === id);
 
-	users = users.filter((u) => u.id !== id);
-	res.status(204).end();
+	if (!deleteTarget) {
+		res
+			.status(404)
+			.json({ message: "The user with the specified ID does not exist." });
+	} else {
+		users = users.filter((u) => u.id !== id);
+		res.status(204).end();
+	}
 });
 
 const port = 8000;
